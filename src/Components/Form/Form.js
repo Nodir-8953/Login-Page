2f1fb807103852e0ed5c85e493173bd7ff637c78
Adrenalin <wedeveloper93@gmail.com>
2022-02-01 00:58:32 +0500

    Create form

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Form.css";
+function Form() {
+  return (
+    <div className="form">
+      <div className="line1">
+        <div className="line11"></div>
+      </div>
+      <div className="line2">
+        <div className="line21"></div>
+      </div>
+      <div className="line3">
+        <div className="line31"></div>
+      </div>
+      <div className="line4">
+        <div className="line41"></div>
+      </div>
+      <div className="form-title">
+        <div className="form-title-vxod">вход</div>
+        <div className="form-title-register">Регистрация</div>
+      </div>
+      <div className="form-input">
+        <input
+          type="email"
+          name="email"
+          className="form-control"
+          value="agro@gmail.com"
+        />
+        <br />
+        <input type="password" name="password" className="form-control" />
+      </div>
+    </div>
+  );
+}
+export default Form;
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Main.css";
+import Form from "../Form/Form";
+
+function Main() {
+  return (
+    <div className="Main">
+      <div className="title-form">
+        <div className="title">aerospace</div>
+        <div className="subtitle">agro</div>
+      </div>
+      <Form />
+    </div>
+  );
+}
+export default Main;
